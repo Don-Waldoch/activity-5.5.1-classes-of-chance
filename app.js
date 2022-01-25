35dd66aa337bbdf5b2a39954f74fb88d970dec0c
unknown <waldoch.don@gmail.com>
2022-01-24 19:25:27 -0600

    Version 1.0

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 class Casino {
     // Write code here
+    constructor(name, isFakeCoin) {
+        this.timesPlayed = 0;
+        this.name = name;
+        this.isFakeCoin = isFakeCoin;
+    }
+    playGame(betAmount) {
+        this.timesPlayed++
+        if ((this.isFakeCoin) || (Math.random() < 0.5)) {
+            console.log(`${this.name} wins`)
+        } else {
+            let wonAmount = betAmount * (this.timesPlayed + 1)
+            console.log(`You win ${wonAmount}`)
+        }
+    }
 };
 
 // TESTS
@@ -11,14 +25,12 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
 
 // Extra BONUS TESTS
 /*
